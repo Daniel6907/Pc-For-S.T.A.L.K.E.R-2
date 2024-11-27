c66c8876f2cd7a4529e247b0670c33dc1a81cf2f
classuser37 <classuser37@local.iful.edu.ua>
2024-11-27 10:58:48 +0200

    Add script for Pc-For-S.T.A.L.K.E.R-2

---
+++ index.js
@@ -0,0 +1,19 @@
+let processor = "intel core i7-13700kf";
+let gpu = "Nvidia Geforce RTX 4080";
+let ram = "32 GB DDR5";
+let storage = "SSD NVMe M.2";
+let motherboard = "Asus Rog Strix Z790-E";
+const power = "Corsair RM850x 850W";
+let cooling = "NZXT Kraken X73 RGB";
+let pcCase = "Lian Li Pc-011 Dynamic";
+console.log(`
+    Комплектуючі для Пк:
+    Процесор: ${processor}
+    Відеокарта: ${gpu}
+    Оперативка: ${ram}
+    Ссд: ${storage}
+    Материнська плата: ${motherboard}
+    Блок живлення: ${power}
+    Охолодження процесора: ${cooling}
+    Корпус: ${pcCase}
+    `);
